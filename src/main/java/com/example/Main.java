a90a5f10aa0546a2ecd01dd3fe4cfa417d08c6b1
Kristian Urup <kur@easv.dk>
2026-04-01 14:08:20 +0200

    Adding new lines

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         Rectangle rect = new Rectangle(10, 20);
-        System.out.println("rect tells the system: 'I want to fit in'");
+        System.out.println("\nrect tells the system: 'I want to fit in'\n");
         rect.draw();
         // A Circle is not equal a rectangle
         // Circle circle = rect;
